97a13bb06a760d5b4248390398b4a466079783e4
Juan Manuel Córdoba <juanmaco-97@hotmail.com>
2020-02-17 14:59:32 -0500

    Se cambia la camara a una panoramica
    Nada que decir

---
+++ New Unity Project/Assets/Oso/Scripts/Rotacion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed = 0.1f;
+    public float rotationSpeedNegative = -0.1f;
+    private float rotationY = 0.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(0, rotationSpeedNegative, 0);
+        }
+
+    }
+}
